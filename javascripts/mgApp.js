fd18a2edb10ed36270a78b5ee672981315b2a246
Anirudh58 <anirudh.jan97@gmail.com>
2016-03-27 14:55:43 +0530

    Game complete. high scores to be done

--- javascripts/mgApp.js
+++ javascripts/mgApp.js
@@ -7,27 +7,38 @@ app.controller('MainController', ['$scope',
         vm.dimension=0;
         vm.grid=[];
         vm.array=[];
-        vm.initialize = function() {
-            console.log(vm.dimension);
+        vm.clicks=0;
+        vm.difficulty="";
+        vm.GameDone = false;
 
+        vm.initialize = function() {
+            $('#myModal').modal({ show: false})
+            console.log(vm.dimension);
             for (var i = 0; i < vm.dimension; i++) {
                 vm.array.push(0);
             }
-
             for (var i = 0; i < vm.dimension; i++) {
                 vm.grid.push(vm.array);
             }
-
             console.log(vm.grid);
+            vm.drawBoard();
+            triggerRandomClicks();
+            vm.clickFunc();
+        };
 
-
+        vm.drawBoard = function(){
             //var lastClicked;
             var grid = clickableGrid(vm.dimension, vm.dimension, function (el, row, col, i) {
                 console.log("You clicked on element:", el);
                 console.log("You clicked on row:", row);
                 console.log("You clicked on col:", col);
-                console.log("You clicked on item #:", i);
 
+                if(isGameDone()){
+                    vm.GameDone = true;
+                    $('#myModal').modal('show');
+                    
+                    //do popup
+                }
             });
 
             document.body.appendChild(grid);
@@ -36,12 +47,12 @@ app.controller('MainController', ['$scope',
                 var i = 0;
                 var grid = document.createElement('table');
                 grid.className = 'grid';
+                $(grid).attr('id', 'biggrid');
                 for (var r = 0; r < rows; ++r) {
                     var tr = grid.appendChild(document.createElement('tr'));
                     for (var c = 0; c < cols; ++c) {
                         var cell = tr.appendChild(document.createElement('td'));
-                        //cell.innerHTML = vm.grid[r][c];
-                        cell.className = 'red';
+                        cell.className = 'green';
                         cell.addEventListener('click', (function (el, r, c, i) {
                             return function () {
                                 vm.grid[r][c] = 1-vm.grid[r][c];
@@ -58,7 +69,6 @@ app.controller('MainController', ['$scope',
                                     vm.grid[r-1][c] = 1-vm.grid[r-1][c];
                                 }
 
-                                //$(el).html(vm.grid[r][c]);
                                 if($(el).attr('class') == 'red'){
                                     $(el).attr('class', 'green');
                                 }
@@ -117,15 +127,70 @@ app.controller('MainController', ['$scope',
                                 }
 
                                 /*el.nextSibling.className='green';
-                                el.previousSibling.className='green';*/
+                                 el.previousSibling.className='green';*/
                                 callback(el, r, c, i);
                             }
                         })(cell, r, c, i), false);
                     }
                 }
+
                 return grid;
             }
+        };
+
+        vm.clickFunc = function(){
+            document.getElementById('biggrid').addEventListener("click", function(){
+                if(!GameDone){
+                    vm.clicks++;
+                }
+                $scope.$apply();
+                console.log('table clicked ', vm.clicks);
+            });
+        };
+
+        var triggerRandomClicks = function(){
+            var grid = document.getElementById('biggrid');
+
+            console.log('grid ka children ', $(grid).children());
+            console.log('children ka children ', $(grid).children().children().eq(10));
+
+            var times;
+
+            switch(vm.difficulty){
+                case 'Easy': times = 10; break;
+                case 'Medium': times = 12; break;
+                case 'Difficult': times = 14; break;
+            }
+
+            for(var i=0;i<times;i++){
+                var temp = Math.floor(Math.random()*63);
+                console.log('randomly clicked', temp);
+                $(grid).children().children().eq(temp).click();
+            }
+
+        };
+
+        function isGameDone(){
+            var grid = document.getElementById('biggrid');
+
+            var flag=0;
+
+            for(var i=0; i<vm.dimension*vm.dimension; i++){
+                if($(grid).children().children().eq(i).attr('class') == 'red'){
+                    flag=1;
+                }
+            }
+
+            if(flag==0){
+                console.log('game over');
+                return true;
+            }
+            else {
+                console.log('game on!');
+                return false;
+            }
         }
+
     }
 ]);
 
